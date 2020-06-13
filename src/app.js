5e4411e8bf89066dbf88e0deb9295ff9e05c4cbc
dawood6 <dawoodsadiq171@gmail.com>
2020-06-13 22:46:39 +0500

    updated code

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import Parent from './Parent';
 import ValueContext from './ValueConttext';
 
 const App = () => {
-    // let [number, setNumber] = useState(6);
     let Value = 81;
     return (
         <ValueContext.Provider value={Value}>
